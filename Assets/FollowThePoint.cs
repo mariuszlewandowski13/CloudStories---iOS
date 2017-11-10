a4e23c1e53c28fdd9e165d5f3ea62fda67faa407
Adam Roszyk <funkylogic.studio@gmail.com>
2017-11-10 18:14:06 +0100

    Physics tests

---
+++ Assets/FollowThePoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowThePoint : MonoBehaviour {
+
+    public Transform destintaion; //you fetch it in advance
+    public float sens = 10; //sensetivity
+    
+    // Update is called once per frame
+    public void Update () {
+
+        GetComponent<Rigidbody>().AddForce(destintaion.position - transform.position * sens);// = (destintaion.position - transform.position) * sens;
+    }
+}
